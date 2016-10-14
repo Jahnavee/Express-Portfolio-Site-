93a2c3aa710a7b0947d86052e92c24255b0a407d
jahnavee <Jahnavee.Parmar@MyGeorgian.ca>
2016-10-13 20:48:44 -0400

    minor mods

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,20 @@ var router = express.Router();
 
 /* GET pages. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Welcome!' });
 });
 
  router.get('/home', function(req, res, next) {
    res.render('home', { title: 'Home' });
  });
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'contact me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 router.get('/service', function(req, res, next) {
-  res.render('service', { title: 'service' });
+  res.render('service', { title: 'Service' });
 });
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'about me' });
+  res.render('about', { title: 'About Me' });
 });
 
 module.exports = router;
